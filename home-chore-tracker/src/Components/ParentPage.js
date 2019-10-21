55315dddbc2592fa9f5dd4a95a7b443771521fa1
Akak Almazbekova <akak.almazbekova@gmail.com>
2019-10-21 16:30:27 -0700

    Added axiosWithAuth and links to App.js

--- home-chore-tracker/src/Components/ParentPage.js
+++ home-chore-tracker/src/Components/ParentPage.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axiosWithAuth from "./utils/axiosWithAuth";
 
 
-function ParentPage() {
-  return (
-    <div className="parent-page">
-     <h1>Welcome to Parent Home Chore Tracker</h1>
-    </div>
-  );
+const ParentPage = () => {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+    axiosWithAuth()
+    .get("//api/users/all")
+    .then(res => {
+      console.log(res.data);
+      setTasks(res.data);
+    }).catch(err =>  console.err(err))
+    }, [])
+    return (
+        <div className="parent-page">
+         <h1>Welcome to Parent Home Chore Tracker</h1>
+        </div>
+      );
+
 }
+
 
 export default ParentPage;
